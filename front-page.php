3ab0478741d3151273ea98badc5d89b43c4c840c
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-05 14:27:38 -0500

    Set up SASS config. options. Started building out global/base/layout styles. Updated 'front-page' markup.

--- front-page.php
+++ front-page.php
@@ -1,9 +1,16 @@
 <?php
 get_header();
 ?>
-
-<h1>This is the 'front-page.php' file.</h1>
-
+<header>
+    <section class="index-hero">
+        <img src="https://placeholdit.imgix.net/~text?txtsize=72&txt=INDEX%20HERO&w=1600&h=900" alt="<?= '/// TEMP - Index Hero'?>">
+    </section><!-- /.index-hero -->
+</header>
+<section class="layout-section">
+    <div class="layout-section__inner">
+        <!-- POSTS HERE -->
+    </div><!-- /.layout-section__inner -->
+</section><!-- /.layout-section -->
 <?php
 get_footer();
 ?>
