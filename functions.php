05358ed6e467e7388ded5a7da4778deab48df74c
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-05 15:17:10 -0500

    Enabled post featured images via 'functions.php'. Updated 'post-preview' partial to pull in/display post thumbnail.

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+/* THEME DEFAULTS */
+add_theme_support( 'post-thumbnails' );
+
 /* THEME STYLES */
 wp_register_style( 'styles', get_stylesheet_directory_uri() . '/css/styles.css' );
 wp_enqueue_style( 'styles' );
--- inc/preview/_preview.php
+++ inc/preview/_preview.php
@@ -1,8 +1,8 @@
 <article class="post-preview">
-    <div class="post-preview__image">
-    </div>
+    <div class="post-preview__image" style="background-image: url( <?php the_post_thumbnail_url(); ?> )">
+    </div><!-- /.post-preview__image -->
     <div class="post-preview__content">
         <h2><?php the_title(); ?></h2>
         <h3><?php echo '/// TEMP - Category'; ?></h3>
-    </div>
+    </div><!-- /.post-preview__content -->
 </article><!-- /.post-preview -->
