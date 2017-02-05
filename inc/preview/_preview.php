a738c9d1e4507beb4679b879651254014e57bd5d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-05 14:58:01 -0500

    Started building out styles for 'post-preview' organism. Completed additional updates to project styles.

--- inc/preview/_preview.php
+++ inc/preview/_preview.php
@@ -1,3 +1,8 @@
 <article class="post-preview">
-    
-</article>
+    <div class="post-preview__image">
+    </div>
+    <div class="post-preview__content">
+        <h2><?php the_title(); ?></h2>
+        <h3><?php echo '/// TEMP - Category'; ?></h3>
+    </div>
+</article><!-- /.post-preview -->
