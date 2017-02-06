43c3220c6abc8173967cadae67e777bdc3092e88
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-05 19:58:26 -0500

    Added 'main.js' script to 'src/' folder. Updated 'functions.php' file to enqueue corresponding minified script.

--- functions.php
+++ functions.php
@@ -5,4 +5,19 @@ add_theme_support( 'post-thumbnails' );
 /* THEME STYLES */
 wp_register_style( 'styles', get_stylesheet_directory_uri() . '/css/styles.css' );
 wp_enqueue_style( 'styles' );
+
+/* THEME SCRIPTS */
+if ( !is_admin() ) {
+    add_action( "wp_enqueue_scripts", "enqueue_jquery", 11 ) ;
+
+    function enqueue_jquery() {
+       wp_deregister_script( 'jquery' );
+       wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js', '', '', false );
+       wp_enqueue_script( 'jquery' );
+    }
+}
+
+
+wp_register_script( 'main', get_stylesheet_directory_uri() . '/js/main.min.js', array( 'jquery' ), '', true );
+wp_enqueue_script( 'main' );
 ?>
