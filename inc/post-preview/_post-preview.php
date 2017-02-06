5e3714b2272cba5f580ff4a1c33a7d203130fa90
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-06 08:52:43 -0500

    Updated 'post-preview' partial file to add supplementary class to text if applicable.

--- inc/post-preview/_post-preview.php
+++ inc/post-preview/_post-preview.php
@@ -1,10 +1,14 @@
+<?php
+$textClass = get_field( 'reverse_text' ) ? 'color--white' : '';
+?>
+
 <article class="post-preview">
     <a href="<?= get_post_permalink(); ?>">
         <div class="post-preview__image" style="background-image: url( <?php the_post_thumbnail_url(); ?> )">
         </div><!-- /.post-preview__image -->
         <div class="post-preview__content">
-            <h2 class="post-preview-title"><?= get_the_title(); ?></h2>
-            <h3 class="post-preview-category"><?= getTheFirstCategory( get_the_ID() );?></h3>
+            <h2 class="post-preview-title <?= $textClass; ?>"><?= get_the_title(); ?></h2>
+            <h3 class="post-preview-category <?= $textClass; ?>"><?= getTheFirstCategory( get_the_ID() );?></h3>
         </div><!-- /.post-preview__content -->
     </a>
 </article><!-- /.post-preview -->
