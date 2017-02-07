ea496b36c999de602d47b45c1da430a15ca4aead
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-06 23:18:13 -0500

    Updated 'functions.php' with theme customization options: 'title'; 'subtitle'.

--- functions.php
+++ functions.php
@@ -1,5 +1,55 @@
 <?php
+/* ACTIONS */
+ add_action( 'customize_register', 'theme_customize_register' );
+
+
 /* THEME FUNCTIONS */
+/**
+ * ...
+ */
+ function theme_customize_register( $wp_customize ) {
+    $wp_customize->add_section( 'sfco-wp-mack_front_page_text' , array(
+        'title' => __( 'Front Page Text', 'sfco-wp-mack' ),
+        'priority' => 9999,
+    ) );
+
+    $wp_customize->add_setting( 'front_page_title' , array(
+        'default' => 'My Site',
+        'transport' => 'refresh',
+    ) );
+
+    $wp_customize->add_setting( 'front_page_subtitle' , array(
+        'default' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'transport' => 'refresh',
+    ) );
+
+    $wp_customize->add_control(
+        new WP_Customize_Control(
+            $wp_customize,
+            'front_page_title',
+            array(
+                'label' => __( 'Title', 'sfco-wp-mack' ),
+                'section' => 'sfco-wp-mack_front_page_text',
+                'settings' => 'front_page_title',
+            )
+        )
+    );
+
+    $wp_customize->add_control(
+        new WP_Customize_Control(
+            $wp_customize,
+            'front_page_subtitle',
+            array(
+                'label' => __( 'Subtitle', 'sfco-wp-mack' ),
+                'section' => 'sfco-wp-mack_front_page_text',
+                'settings' => 'front_page_subtitle',
+                'type' => 'textarea'
+            )
+        )
+    );
+ }
+
+
 /**
  * ...
  *
