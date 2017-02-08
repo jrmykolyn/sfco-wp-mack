ef09d734471193a59740d8e30ccc40bb83c2236d
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-07 19:55:17 -0500

    Updated footer elem. markup, built out initial styles.

--- inc/footer/_footer.php
+++ inc/footer/_footer.php
@@ -1,11 +1,12 @@
 <footer class="footer">
-    <section class="layout-section">
-        <div class="layout-section__inner">
-            <ul>
-                <li>Footer Item 1</li>
-                <li>Footer Item 2</li>
-                <li>Footer Item 3</li>
-            </ul>
-        </div>
-    </section>
+    <div class="footer__inner">
+        <ul class="footer-list">
+            <li class="footer-list__item">
+                <a href="mailto:<?= get_theme_mod( 'contact_info_email' ); ?>"><?= get_theme_mod( 'contact_info_email' ); ?></a>
+            </li>
+            <li class="footer-list__item">
+                <a href="<?= get_theme_mod( 'contact_info_instagram' ); ?>" target="_blank">Instagram</a>
+            </li>
+        </ul>
+    </div>
 </footer>
