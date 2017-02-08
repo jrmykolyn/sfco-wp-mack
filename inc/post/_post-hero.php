2406d72c79e3c39a51f25bf90982f316346b7668
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-07 21:49:29 -0500

    Updated custom field references in 'post hero' component.

--- inc/post/_post-hero.php
+++ inc/post/_post-hero.php
@@ -1,5 +1,5 @@
 <header>
     <section class="post-hero">
-        <img src="<?= get_field( 'hero_image' ); ?>" alt="<?= get_field( 'hero_image_text' ); ?>">
+        <img src="<?= get_field( 'project_hero_image' ); ?>" alt="<?= get_field( 'project_hero_image_text' ); ?>">
     </section><!-- /.index-hero -->
 </header>
