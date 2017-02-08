1588d6616025404b170193fbdc2f263c2751cb91
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-02-07 20:16:43 -0500

    Updated 'footer' partial to loop over and display all 'social' fields.

--- inc/footer/_footer.php
+++ inc/footer/_footer.php
@@ -1,12 +1,47 @@
+<?php
+/// TODO:
+// - pull logic below into dedicated function(s).
+$social_fields = array(
+    'contact_info_instagram',
+    'contact_info_linkedin'
+);
+
+$social_values = array();
+
+foreach( $social_fields as $field ) {
+    $key = explode( '_', $field );
+
+    $val = get_theme_mod( $field );
+
+    if ( $val ) {
+        $social_values[ $key[ 2 ] ] = $val;
+    }
+}
+?>
+
 <footer class="footer">
     <div class="footer__inner">
         <ul class="footer-list">
             <li class="footer-list__item">
                 <a href="mailto:<?= get_theme_mod( 'contact_info_email' ); ?>"><?= get_theme_mod( 'contact_info_email' ); ?></a>
             </li>
-            <li class="footer-list__item">
-                <a href="<?= get_theme_mod( 'contact_info_instagram' ); ?>" target="_blank">Instagram</a>
-            </li>
         </ul>
+        <?php
+        if ( count( $social_values ) ):
+        ?>
+        <ul class="footer-list">
+            <?php
+            foreach( $social_values as $key => $val ):
+            ?>
+            <li class="footer-list__item">
+                <a href="<?= $val; ?>" target="_blank"><?= ucfirst( $key ); ?></a>
+            </li>
+            <?php
+            endforeach;
+            ?>
+        </ul>
+        <?php
+        endif;
+        ?>
     </div>
 </footer>
