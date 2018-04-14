41a045d825c386f35eca8aee03d54f027b9ef297
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-04-14 14:52:38 -0400

    topic-3 chore(gulp): Implement 'PathMap'.

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,25 @@ var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var PathMap = require('sfco-path-map');
 
 
 /* DECLARE VARS */
-var PATHS = {
-    templates: {
-        src: 'src/templates/',
-        dest: './dist/'
-    },
-    styles: {
-        src: 'src/sass/',
-        dest: './dist/css/',
-    },
-    js: {
-        theme: {
-            src: 'src/js/theme/**/*.js',
-            dest: './dist/js/',
-        },
-        vendor: {
-            src: 'src/js/vendor/**/*.js',
-            dest: './dist/js/',
-        },
-    },
-};
+var PATHS = new PathMap( {
+    src: 'src',
+    dest: 'dist',
+    // Templates
+    templatesSrc: '{{src}}/templates',
+    templatesDest: '{{dest}}',
+    // Styles
+    stylesSrc: '{{src}}/sass',
+    stylesDest: '{{dest}}/css',
+    // Scripts
+    scriptsSrc: '{{src}}/js',
+    scriptsDest: '{{dest}}/js',
+    themeScriptsSrc: '{{scriptsSrc}}/theme',
+    vendorScriptsSrc: '{{scriptsSrc}}/vendor',
+} );
 
 
 /* DECLARE FUNCTIONS */
@@ -81,8 +77,8 @@ gulp.task( 'meta', function() {
 gulp.task( 'templates', function() {
     console.log( 'INSIDE TASK: `templates`' );
 
-    gulp.src( PATHS.templates.src + '**/*.php' )
-        .pipe( gulp.dest( PATHS.templates.dest ) );
+    gulp.src( `${PATHS.templatesSrc}/**/*.php` )
+        .pipe( gulp.dest( PATHS.templatesDest ) );
 } );
 
 
@@ -95,7 +91,7 @@ gulp.task( 'templates', function() {
 gulp.task( 'sass', function() {
     console.log( 'INSIDE TASK: `sass`' );
 
-    return gulp.src( PATHS.styles.src + 'styles.scss' )
+    return gulp.src( `${PATHS.stylesSrc}/styles.scss` )
         .pipe( sass(
             {
                 outputStyle: 'compressed',
@@ -107,7 +103,7 @@ gulp.task( 'sass', function() {
                 ]
             }).on( 'error', sass.logError )
         )
-        .pipe( gulp.dest( PATHS.styles.dest ) );
+        .pipe( gulp.dest( PATHS.stylesDest ) );
 } );
 
 
@@ -115,7 +111,7 @@ gulp.task( 'sass', function() {
  * Task concatenates, minifies, and renames all theme scripts.
  */
 gulp.task( 'scripts:theme', function() {
-    return doScripts( PATHS.js.theme.src, PATHS.js.theme.dest, 'theme.js' );
+    return doScripts( `${PATHS.themeScriptsSrc}/**/*.js`, PATHS.scriptsDest, 'theme.js' );
 } );
 
 
@@ -123,7 +119,7 @@ gulp.task( 'scripts:theme', function() {
  * Task concatenates, minifies, and renames all vendor scripts.
  */
 gulp.task( 'scripts:vendor', function() {
-    return doScripts( PATHS.js.vendor.src, PATHS.js.theme.dest, 'vendor.js' );
+    return doScripts( `${PATHS.vendorScriptsSrc}/**/*.js`, PATHS.scriptsDest, 'vendor.js' );
 } );
 
 
@@ -140,7 +136,7 @@ gulp.task( 'scripts', [ 'scripts:vendor', 'scripts:theme' ] );
 gulp.task( 'watch', function() {
     console.log( 'INSIDE TASK: `watch`' );
 
-    gulp.watch( PATHS.templates.src + '**/*.php', [ 'templates' ] );
-    gulp.watch( PATHS.styles.src + '**/*.scss', [ 'sass' ] );
-    gulp.watch( PATHS.js.src + '**/*.scss', [ 'scripts' ] );
+    gulp.watch( `${PATHS.templatesSrc}/**/*.php`, [ 'templates' ] );
+    gulp.watch( `${PATHS.stylesSrc}/**/*.scss`, [ 'sass' ] );
+    gulp.watch( `${PATHS.scriptsSrc}/**/*.js`, [ 'scripts' ] );
 } );
