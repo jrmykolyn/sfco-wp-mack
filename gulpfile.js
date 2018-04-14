e9df6e2e5e24b1542b01a7a8c8870a962863b37f
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-04-14 14:58:38 -0400

    topic-3 chore(gulp): Update formatting, add documentation, and ES6-ify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-/* IMPORT MODULES */
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var PathMap = require('sfco-path-map');
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+const concat = require('gulp-concat');
+const gulp = require('gulp');
+const PathMap = require('sfco-path-map');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
 
-
-/* DECLARE VARS */
-var PATHS = new PathMap( {
+// --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+const PATHS = new PathMap( {
     src: 'src',
     dest: 'dist',
     // Templates
@@ -24,8 +27,9 @@ var PATHS = new PathMap( {
     vendorScriptsSrc: '{{scriptsSrc}}/vendor',
 } );
 
-
-/* DECLARE FUNCTIONS */
+// --------------------------------------------------
+// DECLARE FUNCTIONS
+// --------------------------------------------------
 /**
  * Consume, concatenate, rename, and migrate theme/vendor scripts.
  *
@@ -44,53 +48,34 @@ const doScripts = ( src, dest, name ) => {
         .pipe( gulp.dest( dest ) );
 }
 
-
-/* DECLARE TASKS */
+// --------------------------------------------------
+// DECLARE TASKS
+// --------------------------------------------------
 /**
- * 'Default' Gulp task, executed when `gulp` is run from the
- * command line with *no* arguments.
- *
- * The following tasks are executed *before* the contents of
- * the `default` task.
- * - `sass`
- * - `scripts`
- * - `watch`
+ * 'Default' Gulp task, executed when `gulp` is run from the command line with *no* arguments.
  */
-gulp.task( 'default', [ 'meta', 'templates', 'sass', 'scripts', 'watch' ], function() {
-    console.log( 'INSIDE TASK: `default`' );
-} );
-
+gulp.task( 'default', [ 'meta', 'templates', 'sass', 'scripts', 'watch' ] );
 
 /**
- * ...
+ * Task migrates `style.css` file (required by theme).
  */
 gulp.task( 'meta', function() {
-    console.log( 'INSIDE TASK: `meta`' );
-
-    gulp.src( './src/style.css' )
-        .pipe( gulp.dest( './dist/' ) );
+    gulp.src( `${PATHS.src}/style.css` )
+        .pipe( gulp.dest( PATHS.dest ) );
 } );
 
 /**
- * ...
+ * Task migrates all template/PHP files.
  */
 gulp.task( 'templates', function() {
-    console.log( 'INSIDE TASK: `templates`' );
-
     gulp.src( `${PATHS.templatesSrc}/**/*.php` )
         .pipe( gulp.dest( PATHS.templatesDest ) );
 } );
 
-
 /**
- * Task converts contents of `styles.scss` file (plus any
- * `*.scss` linked via `@import)` to vanilla CSS.
- *
- * Resulting CSS file is saved to specified 'dest' directory
+ * Task converts contents of `styles.scss` file to vanilla CSS.
  */
 gulp.task( 'sass', function() {
-    console.log( 'INSIDE TASK: `sass`' );
-
     return gulp.src( `${PATHS.stylesSrc}/styles.scss` )
         .pipe( sass(
             {
@@ -106,14 +91,12 @@ gulp.task( 'sass', function() {
         .pipe( gulp.dest( PATHS.stylesDest ) );
 } );
 
-
 /**
  * Task concatenates, minifies, and renames all theme scripts.
  */
 gulp.task( 'scripts:theme', function() {
     return doScripts( `${PATHS.themeScriptsSrc}/**/*.js`, PATHS.scriptsDest, 'theme.js' );
 } );
-
 
 /**
  * Task concatenates, minifies, and renames all vendor scripts.
@@ -122,20 +105,15 @@ gulp.task( 'scripts:vendor', function() {
     return doScripts( `${PATHS.vendorScriptsSrc}/**/*.js`, PATHS.scriptsDest, 'vendor.js' );
 } );
 
-
 /**
  * Wrapper around script-related tasks.
  */
 gulp.task( 'scripts', [ 'scripts:vendor', 'scripts:theme' ] );
 
-
 /**
- * Task watches for changes to files in `src/` directory,
- * executes appropriate task.
+ * Task watches for changes to files in `src/` directory, executes appropriate task.
  */
 gulp.task( 'watch', function() {
-    console.log( 'INSIDE TASK: `watch`' );
-
     gulp.watch( `${PATHS.templatesSrc}/**/*.php`, [ 'templates' ] );
     gulp.watch( `${PATHS.stylesSrc}/**/*.scss`, [ 'sass' ] );
     gulp.watch( `${PATHS.scriptsSrc}/**/*.js`, [ 'scripts' ] );
