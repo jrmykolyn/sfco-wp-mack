cf3513b9fa04f1e0576c5317af6e4958617f72be
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-04-26 08:27:28 -0400

    Update 'post hero' component to: account for gif assets; use larger images.

--- theme/inc/post/_post-hero.php
+++ theme/inc/post/_post-hero.php
@@ -3,6 +3,8 @@
     $img = get_field( 'project_hero_image' );
     $img_src;
     $img_srcset;
+    $is_gif;
+    $thumbnail_size = 'full';
 
     // Conditionals below handle cases where `project_hero_image` is one of:
     // - a hash (array).
@@ -10,11 +12,19 @@
     // - a URL (string).
     /// TODO: Update logic and remove fallbacks when custom field settings are added to version control.
     if ( is_array( $img ) && isset( $img[ 'id' ] ) ) {
-        $img_src = wp_get_attachment_image_src( $img[ 'id' ], 'medium_large' )[ 0 ];
-        $img_srcset = wp_get_attachment_image_srcset( $img[ 'id' ], 'medium_large' );
+
+        $is_gif = get_post_mime_type( $img[ 'id' ] ) === 'image/gif';
+        $thumbnail_size = $is_gif ? 'full' : $thumbnail_size;
+        $img_src = wp_get_attachment_image_src( $img[ 'id' ], $thumbnail_size )[ 0 ];
+        $img_srcset = wp_get_attachment_image_srcset( $img[ 'id' ], $thumbnail_size );
+
     } else if ( preg_match( '/[0-9]+/', $img ) ) {
-        $img_src = wp_get_attachment_image_src( $img, 'medium_large' )[ 0 ];
-        $img_srcset = wp_get_attachment_image_srcset( $img, 'medium_large' );
+
+        $is_gif = get_post_mime_type( $img ) === 'image/gif';
+        $thumbnail_size = $is_gif ? 'full' : $thumbnail_size;
+        $img_src = wp_get_attachment_image_src( $img, $thumbnail_size )[ 0 ];
+        $img_srcset = wp_get_attachment_image_srcset( $img, $thumbnail_size );
+
     } else {
         $img_src = $img;
     }
@@ -22,7 +32,12 @@
     <section class="post-hero">
         <img
             src="<?= $img_src; ?>"
-            <?php if ( isset( $img_srcset ) ) { ?>
+            <?php
+            // Include `srcset` if:
+            // - It exists.
+            // - The asset is not a gif (https://wpbloggingnerd.org/solved-gif-not-animating-in-wordpress-posts-and-featured-images/).
+            if ( isset( $img_srcset ) && ! $is_gif ) {
+            ?>
                 srcset="<?= $img_srcset; ?>"
                 sizes="100vw"
             <?php } ?>
