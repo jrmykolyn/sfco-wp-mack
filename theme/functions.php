28984e95f245b027e21710a9584e13b3eef2c72c
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-04-26 08:21:15 -0400

    Add 'hero' image size (and variants).

--- theme/functions.php
+++ theme/functions.php
@@ -265,6 +265,11 @@ function getTheFirstCategory( $id=-1, $nameOnly=true) {
 /* THEME DEFAULTS */
 add_theme_support( 'post-thumbnails' );
 
+/* IMAGES */
+add_image_size( 'hero--small', 1200, 675, false );
+add_image_size( 'hero', 1600, 900, false );
+add_image_size( 'hero--large', 2000, 1125, false );
+add_image_size( 'hero--xlarge', 2400, 1350, false );
 
 /* THEME SCRIPTS/STYLES */
 if ( !is_admin() ) {
