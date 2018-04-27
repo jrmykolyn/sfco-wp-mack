364dbb954b4c71cf4b8990ac88740c15b38df457
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-04-27 08:52:49 -0400

    Move front page 'hero' component into dedicated 'index hero' partial.

--- theme/front-page.php
+++ theme/front-page.php
@@ -2,20 +2,7 @@
 get_header();
 ?>
 <header>
-    <section class="index-hero">
-        <div class="index-hero__image">
-            <div class="index-hero__image__background" style="background-image: url( <?= get_theme_mod( 'front_page_image_background' ); ?> );"></div>
-            <div class="index-hero__image__midground" style="background-image: url( <?= get_theme_mod( 'front_page_image_midground' ); ?> );"></div>
-            <div class="index-hero__image__foreground" style="background-image: url( <?= get_theme_mod( 'front_page_image_foreground' ); ?> );"></div>
-        </div>
-        <div class="index-hero__content">
-            <div class="index-hero-text">
-                <h1 class="index-title"><?= get_theme_mod( 'front_page_title' ); ?></h2>
-                <h2 class="index-subtitle"><?= get_theme_mod( 'front_page_subtitle' ); ?></h3>
-            </div>
-        </div>
-        <div class="index-hero__cta js--index-hero-arrow"></div>
-    </section><!-- /.index-hero -->
+    <?php get_template_part( 'inc/index-hero/_index-hero' ); ?>
 </header>
 <section class="layout-section">
     <div class="layout-section__inner">
---
+++ theme/inc/index-hero/_index-hero.php
@@ -0,0 +1,14 @@
+<section class="index-hero">
+    <div class="index-hero__image">
+        <div class="index-hero__image__background" style="background-image: url( <?= get_theme_mod( 'front_page_image_background' ); ?> );"></div>
+        <div class="index-hero__image__midground" style="background-image: url( <?= get_theme_mod( 'front_page_image_midground' ); ?> );"></div>
+        <div class="index-hero__image__foreground" style="background-image: url( <?= get_theme_mod( 'front_page_image_foreground' ); ?> );"></div>
+    </div>
+    <div class="index-hero__content">
+        <div class="index-hero-text">
+            <h1 class="index-title"><?= get_theme_mod( 'front_page_title' ); ?></h2>
+            <h2 class="index-subtitle"><?= get_theme_mod( 'front_page_subtitle' ); ?></h3>
+        </div>
+    </div>
+    <div class="index-hero__cta js--index-hero-arrow"></div>
+</section><!-- /.index-hero -->
